3b5e8082ead54346724c1edf595ad902f908c7a0
Ellies1 <elliesgood00@163.com>
2024-05-14 15:23:33 +0200

    Try to fix a bug

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/seeds', async (req, res) => {
 });
 
 
-app.post('/seeds/score', express.json(), async (req, res) => {
+app.post('/seeds', express.json(), async (req, res) => {
   const { selectedSeedName, rating } = req.body;
   try {
     const updatedSeed = await Seed.findOneAndUpdate(
