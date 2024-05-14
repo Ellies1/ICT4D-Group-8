dbd843b2af7e9f2869853c019a96685bbb662e20
Ellies1 <elliesgood00@163.com>
2024-05-14 15:41:23 +0200

    try to fix a bug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
